db802fb30d6005a53d7e630e3885ed6f92e126db
KalioDragon <lemlih.kamal@emsi-edu.ma>
2016-11-10 23:10:08 +0000

    v1.2

---
+++ database/migrations/2016_11_10_213213_create_personnes_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonnesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personnes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('adresse');
+            $table->string('email');
+            $table->string('tel');
+            $table->string('annee');
+            $table->string('formation');
+            $table->string('frais');
+            $table->string('etablissement');
+            $table->string('emplois');
+            $table->string('etabissement');
+            $table->string('employeur');
+            $table->string('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personnes');
+    }
+}
---
+++ database/migrations/2016_11_10_213423_create_examens_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExamensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('examens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->string('heure');
+            $table->integer('module_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('examens');
+    }
+}
